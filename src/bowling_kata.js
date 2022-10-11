8fd0d1f806f1d1c0f18ffdeb0d68a051087e1adb
TaChyla <tachyla.murray@leadingagile.com>
2022-10-11 15:02:50 -0400

    Player scores are stored in an object for p1 & p2

--- src/bowling_kata.js
+++ src/bowling_kata.js
@@ -1,5 +1,11 @@
 module.exports = class BowlingGame{
+    playerScores = {
+        p1: [],
+        p2: []
+    };
+
     getScore(){
+
         return '0 - 0';
     }
 }
--- test/bowling_kata.test.js
+++ test/bowling_kata.test.js
@@ -1,9 +1,22 @@
 const BowlingGame = require('../src/bowling_kata');
 describe('tests bowling game', () => {
-    it('returns true', () => {
+    it('returns game score', () => {
         const testBowlingGame = new BowlingGame();
         result = testBowlingGame.getScore();
 
         expect(result).toBe('0 - 0');
+    });
+
+    it('returns object holding game scores', () => {
+        const testBowlingGame = new BowlingGame();
+
+       let player_scores = testBowlingGame.playerScores;
+
+       expect(player_scores).toStrictEqual({
+        p1: [], 
+        p2: []
+    });
+
+        
     });
 });
