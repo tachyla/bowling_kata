90551e750e67434776f7e2b0899f60e77af23165
TaChyla <tachyla.murray@leadingagile.com>
2022-10-19 13:52:18 -0400

    returns playerScores last element in array

--- bowlingGame.test.js
+++ bowlingGame.test.js
@@ -3,10 +3,17 @@ const Game = require('./src/bowlingGame');
 
 describe('bowling game', () => {
 
-    it("adds point value from player throw", () => {
+    it('adds 1 point value from player throw', () => {
         const testGame = new Game();
-        let result = testGame.recordThrow("player1", 1);
+        let result = testGame.recordThrow('player1', 1);
 
         expect(result).toEqual(1);
+    });
+
+    it('adds 9 points value from player throw', () => {
+        const testGame = new Game();
+        let result = testGame.recordThrow('player1', 9);
+
+        expect(result).toEqual(9);
     });
 })
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -9,7 +9,8 @@ module.exports = class Game {
     recordThrow = (playerValue, pinValue) => {
         if(playerValue === this.players[0].name){
             let playerScore = this.players[0].score;
-            return playerScore.push(pinValue);
+            playerScore.push(pinValue);
+            return playerScore.pop();
         };
     }
 
