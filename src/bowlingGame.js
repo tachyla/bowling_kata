4c1a07109e51cdbcefd20482a46fa05fa6f5677f
TaChyla <tachyla.murray@leadingagile.com>
2022-10-24 09:54:32 -0400

    removed use of scores array

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,7 +1,6 @@
 module.exports = class Game {
 
     constructor(){
-        this._scores = [];
         this._frames = [];
         for(let i = 0; i < 10; i++){
             this._frames.push({frameNumber: i, first_roll: null, second_roll: null});
@@ -12,10 +11,8 @@ module.exports = class Game {
     recordThrow = (pinValue) => { 
 
         if(pinValue > 10 || pinValue < 0){
-            throw new RangeError("Valid rolls are 0 - 10");
+            throw new RangeError("Valid throws are 0 - 10");
         }
-
-        this._scores.push(pinValue);
         
         if(!this._frames[this.currentFrame].first_roll){
             this._frames[this.currentFrame].first_roll = pinValue;
@@ -25,7 +22,7 @@ module.exports = class Game {
         let frameTotal = this._frames[this.currentFrame].first_roll + pinValue;
 
         if(frameTotal > 10 ){
-            throw new Error("Invalid roll combination");
+            throw new Error("Invalid throw combination");
         }
         
         this._frames[this.currentFrame].second_roll = pinValue;
@@ -38,10 +35,6 @@ module.exports = class Game {
 
     get frames(){
         return [...this._frames];
-    }
-
-    get scores(){
-        return [...this._scores];
     }
 }
 
--- test/bowlingGame.test.js
+++ test/bowlingGame.test.js
@@ -5,10 +5,10 @@ describe('bowling game', () => {
     it('adds 1 point value from player throw', () => {
         const testGame = new Game();
         testGame.recordThrow(1);
-        expect(testGame.scores).toEqual([1]);
+        expect(testGame.frames[0].first_roll).toEqual(1);
     });
 
-    it('throws error when outside the valid range of a roll', () => {
+    it('throws error when outside the valid range of a throw', () => {
         const testGame = new Game();
 
         expect(() => {
@@ -17,15 +17,7 @@ describe('bowling game', () => {
 
     });
 
-    it('restricts score from being mutated outside of recordThrow method', () => {
-        const testGame = new Game();
-        testGame.recordThrow(10);
-        testGame.scores.push(1);
-
-        expect(testGame.scores).toEqual([10]);
-    });
-
-    it('frame 1 allows two throws', () => {
+    it('records two throws per one frame', () => {
         const testGame = new Game();
         testGame.recordThrow(5);
         testGame.recordThrow(4);
@@ -44,12 +36,12 @@ describe('bowling game', () => {
         expect(testGame.calculateFrameScore()).toEqual(2);
     });
 
-    it('throws error for invalid roll combinations', () => {
+    it('throws error for invalid throw combinations', () => {
         const testGame = new Game();
         testGame.recordThrow(5);
         
         expect(() => {
-            testGame.recordThrow(6)}).toThrow(Error("Invalid roll combination"));
+            testGame.recordThrow(6)}).toThrow(Error("Invalid throw combination"));
     });
 
     it('has 10 frames for a new game', () => {
@@ -57,7 +49,7 @@ describe('bowling game', () => {
         expect(testGame.frames.length).toEqual(10);
     });
 
-    it('records the first roll in the first frame', () => {
+    it('records the first throw in the first frame', () => {
         const testGame = new Game();
         testGame.recordThrow(5);
         expect(testGame.frames[0]).toEqual({frameNumber: 0, first_roll: 5, second_roll: null});
